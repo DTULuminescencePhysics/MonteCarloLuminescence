f50ec1f976761b53c0d0248abb69f5730cbd76ce
Oliver Bramley <olaibr@dtu.dk>
2025-12-17 10:44:28 +0100

    Commented out the thermo chronology section of the main function as this is not needed for core model development

--- src/main_function.py
+++ src/main_function.py
@@ -7,7 +7,8 @@ from src.filesystem import CONFIG_DIR
 from src.helper_functions import cfg_list_check
 from src.MC_analytic_control import monte_carlo_control_functions, analytic_control_functions
 from src.process_plot import plot_analytic_comp_MC
-from src.back_tracing import MC_control_functions,RJMCMC_control_functions
+
+# from src.back_tracing import MC_control_functions,RJMCMC_control_functions 
 
 
 @hydra.main(config_path=CONFIG_DIR,config_name="config", version_base=None)
@@ -16,8 +17,7 @@ def main(cfg: DictConfig):
     err = next(h for h in root.handlers if isinstance(h, ErrorOutputHandler))
 
     run_num, runs = cfg_list_check(cfg,err)
-    run_num=1 
-    runs = runs[0]
+    
     err.checkpoint()
     if cfg.mc.mc:
         mc_file = monte_carlo_control_functions(runs,run_num,err)
@@ -27,5 +27,9 @@ def main(cfg: DictConfig):
     if 'mc_file' in locals() and 'ac_file' in locals():
         plot_analytic_comp_MC(run_num,ac_file,mc_file,cfg.temp.unit)
     
-    RJMCMC_control_functions(runs,run_num,err,ac_file)
+    ###############################################################################################
+    # The following functions are the thermo chronology section of code and have been commented   #
+    # out for working on the main model                                                           #
+    ###############################################################################################
+    # RJMCMC_control_functions(runs,run_num,err,ac_file)
     # MC_control_functions(runs,run_num,err,ac_file)
